b034556207cb740893a9f5b3afcb18db98f2aed5
M.Jovanka Slavia Wijaya <Jonathanss999@gmail.com>
2025-11-10 17:01:25 +0700

    Menambahkan komponen lokasi perumahan

--- griya-mdp/src/app/home/home.ts
+++ griya-mdp/src/app/home/home.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { LokasiPerumahan } from '../lokasi-perumahan/lokasi-perumahan';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [LokasiPerumahan],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
+  numbers: number []= [1,2,3,4,5]
 
 }
